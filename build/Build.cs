bc803f8164fd3364705576501561263cd8fa9b35
Max Katz <maxkatz6@outlook.com>
2025-08-28 13:59:52 -0700

    Remove dotnet-ilrepack

--- build/Build.cs
+++ build/Build.cs
@@ -25,8 +25,6 @@ class Build : NukeBuild
         Execute<Build>(x => x.CopyPackagesToNuGetCache) :
         Execute<Build>(x => x.CreateNugetPackages);
 
-    [NuGetPackage("dotnet-ilrepack", "ILRepackTool.dll", Framework = "net8.0")] readonly Tool IlRepackTool;
-
     [NuGetPackage("Babel.Obfuscator.Tool", "babel.dll", Framework = "net9.0")] readonly Tool Babel;
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
@@ -62,44 +60,8 @@ class Build : NukeBuild
             .SetConfiguration(Configuration)
         ));
 
-    Target IlMerge => _ => _
-        .DependsOn(Compile)
-        .DependsOn(RunTests)
-        .Executes(() =>
-        {
-            var mergeRootProjects = (RootDirectory / "src").GlobFiles("**/*.csproj").Where(p =>
-                p.Name.Contains("Avalonia.Controls.WebView.csproj") ||
-                p.Name.Contains("Avalonia.Xpf.Controls.WebView"));
-
-            var libs = string.Join(' ', GetExtraDepLibs().Select(l => $"/lib:{l}"));
-            var coreProjectPublicApi = (RootDirectory / "src").GlobFiles("**/Avalonia.Controls.WebView.Core.csproj")
-                .First().Parent / "public-api.txt";
-
-            foreach (var mergeRootProject in mergeRootProjects)
-            {
-                var projectName = Path.GetFileNameWithoutExtension(mergeRootProject);
-                var mergeRootDlls = mergeRootProject.Parent
-                    .GlobFiles(Path.Combine("bin", Configuration, "**", projectName + ".dll"));
-                foreach (var mergeRootDll in mergeRootDlls)
-                {
-                    string[] depNamesToMerge = ["Avalonia.Controls.WebView.Core.dll", "AvaloniaUI.Licensing.dll"];
-                    var dependenciesToMerge = mergeRootDll.Parent
-                        .GlobFiles("*.dll")
-                        .Where(f => Array.IndexOf(depNamesToMerge, f.Name) >= 0);
-
-                    var dependenciesArg = string.Join(" ", dependenciesToMerge.Select(dll => '"' + dll + '"'));
-                    var signParams = $"/keyfile:{Statics.AvaloniaStrongNameKey}";
-
-                    IlRepackTool.Invoke(
-                        $"""/internalize:{coreProjectPublicApi} /renameinternalized /parallel /ndebug {libs:nq} {signParams} /out:"{mergeRootDll}" "{mergeRootDll}" {dependenciesArg} """,
-                        mergeRootDll.Parent);
-                }
-            }
-        });
-
     Target Obfuscate => _ => _
         .DependsOn(Compile)
-        .DependsOn(IlMerge)
         .Executes(() =>
         {
             string[] projectsToObfuscate =
@@ -110,11 +72,17 @@ class Build : NukeBuild
             foreach (var project in (RootDirectory / "src").GlobFiles("**/*.csproj")
                      .Where(p => projectsToObfuscate.Contains(p.NameWithoutExtension)))
             {
+                List<string> dependencies = ["Avalonia.Controls.WebView.Core"];
+                if (project.NameWithoutExtension != "Avalonia.Xpf.Controls.WebView")
+                {
+                    dependencies.Add("AvaloniaUI.Licensing");
+                }
+
                 var tfms = (project.Parent / "bin" / Configuration).GetDirectories();
                 NukeExtensions.Babel.Obfuscate(
                     Babel,
                     assemblyName: project.NameWithoutExtension,
-                    targets: tfms.Select(tfm => new Babel.ObfuscationTargetFramework(tfm, [])),
+                    targets: tfms.Select(tfm => new Babel.ObfuscationTargetFramework(tfm, dependencies.ToArray())),
                     signKey: Statics.AvaloniaStrongNameKey,
                     licenseFile: Statics.BabelLicense,
                     rulesFiles: [
@@ -128,7 +96,6 @@ class Build : NukeBuild
         .DependsOn(OutputParameters)
         .DependsOn(RunTests)
         .DependsOn(Compile)
-        .DependsOn(IlMerge)
         .DependsOn(Obfuscate)
         .Executes(() =>
         {
@@ -160,18 +127,4 @@ class Build : NukeBuild
             isPackingToLocalCache: RunningTargets.Concat(ScheduledTargets)
                 .Any(t => t.Name == nameof(CopyPackagesToNuGetCache)))
         .ToString();
-
-    static IEnumerable<string> GetExtraDepLibs()
-    {
-        // See https://github.com/gluck/il-repack/issues/399
-        var androidSdk = NuGetPackageResolver.GetGlobalInstalledPackage("Microsoft.Android.Ref.34",
-            new VersionRange(new NuGetVersion(1, 0, 0)), null)?.Directory;
-        if (androidSdk is null)
-        {
-            throw new DirectoryNotFoundException("Unable to find installed \"Microsoft.Android.Ref.34\" nuget package.");
-        }
-
-        var androidRefs = androidSdk / "ref" / "net8.0";
-        yield return androidRefs;
-    }
 }
